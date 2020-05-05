e6d2de3e48ef9bfcf8094aa9b3c86cad1e2d13ec
andresbgiannotta <57359516+andresbgiannotta@users.noreply.github.com>
2020-05-05 13:35:16 -0300

    first comit hope it works!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `WAMI.BIZ Brand Character`,
+    description: `Your Company, your Style.`,
+    author: `@andresbgiannotta`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi! Give a digital voice to your Brand</h1>
+    <h2>In 2020 the most influencial companies are offering premium digital experiences to their users.</h2>
+    <p>Together we can build something great to reflect your real value trough your brand.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
